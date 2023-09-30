6757123fa76183b2f3322e5856d235a9863a17b0
Henk Sandoval <henkalexander.sandoval@gmail.com>
2023-09-30 18:15:50 +0200

    Creado el primer test del servicio de alquiler de peliculas (MovieRental)

---
+++ MovieRentalApi/Services/MovieRentalService.cs
@@ -0,0 +1,12 @@
+﻿using MovieRentalApi.Models;
+
+namespace MovieRentalApi.Services;
+
+public class MovieRentalService
+{
+    public async Task<MovieModel> FindMovieAsync(int idMovie)
+    {
+        var movie = new MovieModel { Id = idMovie };
+        return movie;
+    }
+}
---
+++ MovieRentalApiTests/Services/MovieRentalServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using MovieRentalApi.Models;
+using MovieRentalApi.Services;
+
+namespace MovieRentalApiTests.Services;
+
+using System.Threading.Tasks;
+
+public class MovieRentalServiceTests
+{
+    [Fact]
+    public async Task MovieRentalService_WhenReceivedIdMovie_ShouldReturnTheMovie()
+    {
+        //Arrange
+        int idMovie = 1;
+        var service = new MovieRentalService();
+
+        //Act
+        MovieModel response = await service.FindMovieAsync(idMovie);
+
+        //Assert
+        var expectedResponse = new MovieModel { Id = idMovie };
+        Assert.Equivalent(expectedResponse, response);
+    }
+
+    [Fact]
+    public async Task MovieRentalService_WhenReceivedIdMovie_ShouldReturnTheMovie2()
+    {
+        //Arrange
+        int idMovie = 2;
+        var service = new MovieRentalService();
+
+        //Act
+        MovieModel response = await service.FindMovieAsync(idMovie);
+
+        //Assert
+        var expectedResponse = new MovieModel { Id = idMovie };
+        Assert.Equivalent(expectedResponse, response);
+    }
+}
