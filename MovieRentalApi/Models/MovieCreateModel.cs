7765a8758e922c0e303256e0588a5fd3fa79e59f
Henk Sandoval <henkalexander.sandoval@gmail.com>
2023-09-30 17:24:22 +0200

    Quitado el test unitario para luego hacerlo con WebApplicationFactory

--- MovieRentalApi/Models/MovieCreateModel.cs
+++ MovieRentalApi/Models/MovieCreateModel.cs
@@ -1,8 +1,14 @@
-﻿namespace MovieRentalApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRentalApi.Models
 {
     public class MovieCreateModel
     {
+        //[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        //[StringLength(maximumLength: 20, MinimumLength = 2, ErrorMessage = "El campo {0} debe contener entre {2} y {1} carácteres de longitud.")]
+        [Required]
         public string Title { get; set; }
+
         public string Description { get; set; }
         public int Year { get; set; }
     }
