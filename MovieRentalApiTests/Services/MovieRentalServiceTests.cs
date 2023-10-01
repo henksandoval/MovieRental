b85977d151123118da2a3488d9aca5b2e1e3d87d
Henk Sandoval <henkalexander.sandoval@gmail.com>
2023-10-01 21:05:46 +0200

    Agregado el test unitario de MovieRentalService Cuando la Pelicula Esta Disponible, debe guardar la fecha de alquiler

--- MovieRentalApiTests/Services/MovieRentalServiceTests.cs
+++ MovieRentalApiTests/Services/MovieRentalServiceTests.cs
@@ -2,6 +2,7 @@
 using MovieRentalApi.Mappers;
 using MovieRentalApi.Models;
 using MovieRentalApi.Services;
+using MovieRentalApi.Utilities;
 
 namespace MovieRentalApiTests.Services;
 
@@ -14,10 +15,12 @@ using NSubstitute;
 
 public class MovieRentalServiceTests
 {
-    private readonly Fixture _fixture = new Fixture();
-    private readonly IBaseRepository<MovieEntity> _repository;
-    private readonly MovieRentalService _service;
-    private readonly IMapper _mapper;
+    private readonly Fixture fixture = new Fixture();
+    private readonly IBaseRepository<MovieEntity> repositoryMovie;
+    private readonly IBaseRepository<RentalEntity> repositoryRental;
+    private readonly MovieRentalService service;
+    private readonly IMapper mapper;
+    private readonly IClock clock;
 
     public MovieRentalServiceTests()
     {
@@ -26,9 +29,11 @@ public class MovieRentalServiceTests
             config.AddProfile<MapperProfile>();
         });
 
-        _mapper = new Mapper(configuration);
-        _repository = Substitute.For<IBaseRepository<MovieEntity>>();
-        _service = new MovieRentalService(_repository, _mapper);
+        mapper = new Mapper(configuration);
+        repositoryMovie = Substitute.For<IBaseRepository<MovieEntity>>();
+        repositoryRental = Substitute.For<IBaseRepository<RentalEntity>>();
+        clock = Substitute.For<IClock>();
+        service = new MovieRentalService(repositoryMovie, repositoryRental, clock, mapper);
     }
 
     [Fact(DisplayName = "MovieRentalService Cuando Solicitan Un Id de Pelicula Disponoble, debería responder la pelicula.")]
@@ -37,10 +42,10 @@ public class MovieRentalServiceTests
         //Arrange (Preparar)
         var idMovie = 1;
         var entity = new MovieEntity{ IsAvailable = true };
-        _repository.GetByIdAsync(idMovie).Returns(entity);
+        repositoryMovie.GetByIdAsync(idMovie).Returns(entity);
         
         //Act (Actuar)
-        var movieResponse = await _service.FindMovieAsync(idMovie);
+        var movieResponse = await service.FindMovieAsync(idMovie);
         
         //Assert (Asegurar)
         var expectedMovie = new MovieModel();
@@ -53,10 +58,10 @@ public class MovieRentalServiceTests
         //Arrange (Preparar)
         var idMovie = 1;
         var entity = new MovieEntity{ IsAvailable = false };
-        _repository.GetByIdAsync(idMovie).Returns(entity);
+        repositoryMovie.GetByIdAsync(idMovie).Returns(entity);
         
         //Act (Actuar)
-        var movieResponse = await _service.FindMovieAsync(idMovie);
+        var movieResponse = await service.FindMovieAsync(idMovie);
         
         //Assert (Asegurar)
         movieResponse.Should().BeNull();
@@ -69,12 +74,30 @@ public class MovieRentalServiceTests
         //Arrange (Preparar)
         const int idMovie = 1;
         var entity = new MovieEntity{ IsAvailable = true };
-        _repository.GetByIdAsync(idMovie).Returns(entity);
+        repositoryMovie.GetByIdAsync(idMovie).Returns(entity);
 
         //Act (Actuar)
-        _ = await _service.FindMovieAsync(idMovie);
+        _ = await service.FindMovieAsync(idMovie);
         
         //Assert (Asegurar)
-        await _repository.Received(1).UpdateAsync(Arg.Is<MovieEntity>(e => !e.IsAvailable));
+        await repositoryMovie.Received(1).UpdateAsync(Arg.Is<MovieEntity>(e => !e.IsAvailable));
+    }
+
+    [Fact(DisplayName = "MovieRentalService Cuando la Pelicula Esta Disponible, debe guardar la fecha de alquiler.")]
+    public async Task MovieRentalService_WhenMovieIsAvailable_ShouldSaveRentalDateInDatabase()
+    {
+        //Arrange (Preparar)
+        const int idMovie = 1;
+        var entity = new MovieEntity{ IsAvailable = true };
+        var expectedDate = new DateTime(1995, 05, 16);
+        repositoryMovie.GetByIdAsync(idMovie).Returns(entity);
+        
+        clock.GetCurrentTime().Returns(expectedDate);
+
+        //Act (Actuar)
+        _ = await service.FindMovieAsync(idMovie);
+
+        //Assert (Asegurar)
+        await repositoryRental.Received(1).CreateAsync(Arg.Is<RentalEntity>(r => r.RentalDate == expectedDate));
     }
 }
