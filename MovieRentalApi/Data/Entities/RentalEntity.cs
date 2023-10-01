bb8960df069a24d1752ff97005b9f7f2dc9d1642
Henk Sandoval <henkalexander.sandoval@gmail.com>
2023-10-01 21:06:21 +0200

    Agregada la solución para el test en la clase MovieRentalService.cs

---
+++ MovieRentalApi/Data/Entities/RentalEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieRentalApi.Data.Entities;
+
+public class RentalEntity
+{
+	public int Id { get; set; }
+	public DateTime RentalDate { get; set; }
+	
+	public virtual MovieEntity Movie { get; set; }
+}
--- MovieRentalApi/Services/MovieRentalService.cs
+++ MovieRentalApi/Services/MovieRentalService.cs
@@ -2,30 +2,41 @@
 using MovieRentalApi.Data.Entities;
 using MovieRentalApi.Data.Repositories;
 using MovieRentalApi.Models;
+using MovieRentalApi.Utilities;
 
 namespace MovieRentalApi.Services;
 
 public class MovieRentalService
 {
-    private readonly IBaseRepository<MovieEntity> _repository;
-    private readonly IMapper _mapper;
+    private readonly IBaseRepository<MovieEntity> repositoryMovie;
+    private readonly IBaseRepository<RentalEntity> repositoryRental;
+    private readonly IClock clock;
+    private readonly IMapper mapper;
 
-    public MovieRentalService(IBaseRepository<MovieEntity> repository, IMapper mapper)
+    public MovieRentalService(IBaseRepository<MovieEntity> repositoryMovie,
+        IBaseRepository<RentalEntity> repositoryRental,
+        IClock clock,
+        IMapper mapper)
     {
-        _repository = repository;
-        _mapper = mapper;
+        this.repositoryMovie = repositoryMovie;
+        this.repositoryRental = repositoryRental;
+        this.clock = clock;
+        this.mapper = mapper;
     }
 
     public async Task<MovieModel> FindMovieAsync(int idMovie)
     {
-        var entity = await _repository.GetByIdAsync(idMovie);
+        var movieEntity = await repositoryMovie.GetByIdAsync(idMovie);
 
-        if (!entity.IsAvailable)
+        if (!movieEntity.IsAvailable)
             return null;
 
-        entity.IsAvailable = false;
-        var movie = _mapper.Map<MovieModel>(entity);
-        await _repository.UpdateAsync(entity);
+        movieEntity.IsAvailable = false;
+        var movie = mapper.Map<MovieModel>(movieEntity);
+        var rentalEntity = new RentalEntity { RentalDate = clock.GetCurrentTime() };
+
+        await repositoryMovie.UpdateAsync(movieEntity);
+        await repositoryRental.CreateAsync(rentalEntity);
 
         return movie;
     }
